3a0aa1e0b085ebcdce9f010c13796a0ce1e66156
DmYakovenko <st6274957@stud.duikt.edu.ua>
2024-12-11 23:12:40 +0200

    Updated

---
+++ Ex1.cs
@@ -0,0 +1,25 @@
+﻿namespace Pz1
+{
+    internal class Ex1
+    {
+        private bool Compare(int N, int Number)
+        {
+            if (Number >= 1 && Number <= N)
+                return true;
+            else
+                return false;
+        }
+        public void Exercise(int N)
+        {
+            int[] Numbers = {6, 90, 12};
+
+            foreach (int i in Numbers)
+            {
+                if (Compare(N, i))
+                    Console.WriteLine($"{i} в діапазоні");
+                else
+                    Console.WriteLine($"{i} не в діапазоні");
+            }
+        }
+    }
+}
---
+++ Ex2.cs
@@ -0,0 +1,50 @@
+﻿namespace Pz1
+{
+	internal class Ex2
+	{
+		private double a = 5;
+		private double b = 8;
+		private double c = 10;
+		private double Perimeter(double a, double b, double c)
+		{
+			return a + b + c;
+		}
+
+		private double Area(double p, double a, double b, double c)
+		{
+			return Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+		}
+
+		private void Type(double a, double b, double c)
+		{
+			if (a == b && b == c)
+			{
+				Console.WriteLine("Трикутник ріносторонній");
+			}
+			else if (a == b || b == c || c == a)
+			{
+				Console.WriteLine("Трикутник рівнобедренний");
+			}
+			else
+			{
+				Console.WriteLine("Просто трикутник");
+			}
+		}
+		public void Exercise()
+		{
+
+			if (((a + b) < c || (b + c) < a || (c + a) < b) || (a <= 0 || b <= 0 || c <= 0))
+			{
+				Console.WriteLine("Трикутника не існує");
+				return;
+			}
+
+			Console.WriteLine("Периметр трикутника: " + (Perimeter(a, b, c)));
+
+			double p = Perimeter(a, b, c) / 2;
+			Console.WriteLine("Площа трикутника: " + Area(p, a, b, c));
+
+			Type(a, b, c);
+		}
+	}
+}
