90ce32471b6a511a0282f004de09960983366d8b
DmYakovenko <st6274957@stud.duikt.edu.ua>
2025-02-16 09:58:23 +0200

    SkladApp
    1

---
+++ SkladApp/Product.cs
@@ -0,0 +1,88 @@
+﻿
+namespace SkladApp
+{
+    class Product
+    {
+        private string _name;
+        private decimal _price;
+        private int _quantity;
+
+
+        public Product()
+        {
+            Console.WriteLine("Товар створено.");
+        }
+
+        public Product(string name, decimal price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            _quantity = quantity;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Назва не може бути порожньою.");
+                }
+                _name = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Ціна не може бути від'ємною.");
+                }
+                _price = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+        }
+
+        public decimal TotalValue
+        {
+            get { return _price * _quantity; }
+        }
+
+        public void Restock(int amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Кількість для поповнення має бути додатною.");
+            }
+            _quantity += amount;
+        }
+
+        public void Sell(int amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Кількість для продажу має бути додатною.");
+            }
+            if (amount > _quantity)
+            {
+                Console.WriteLine("Недостатньо товару на складі!");
+                return;
+            }
+            _quantity -= amount;
+        }
+
+        public string GetInfo()
+        {
+            return $"Товар: {Name}, Ціна: {Price} грн, Кількість: {Quantity}, Загальна вартість: {TotalValue} грн";
+        }
+
+    }
+}
---
+++ SkladApp/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace SkladApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Product t1 = new Product("Товар1", 5, 100);
+            Console.WriteLine(t1.GetInfo());
+
+            t1.Sell(20);
+            Console.WriteLine(t1.GetInfo());
+
+            t1.Restock(50);
+            Console.WriteLine(t1.GetInfo());
+
+            t1.Price = 7;
+            Console.WriteLine(t1.GetInfo());
+
+            Product t3 = new Product();
+            Console.WriteLine(t3.GetInfo());
+
+        }
+    }
+}
